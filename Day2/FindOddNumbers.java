cc800de002ce1f21bfa675709e6602c7a77da574
Manimozhiv123 <manimozhi.pm41@gmail.com>
2024-06-19 19:39:49 +0530

    Add files via upload
    Day1

---
+++ Day2/FindOddNumbers.java
@@ -0,0 +1,16 @@
+package Week1.Day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int maxLength = 10;
+		for (int i=0;i <=maxLength;i++){
+
+		if (i%2 == 1)
+			
+			System.out.println(i);
+}
+			}
+
+}
