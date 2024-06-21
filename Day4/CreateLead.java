0629bea499ce13663759b2d18d752ccfa0e1efff
Manimozhiv123 <manimozhi.pm41@gmail.com>
2024-06-21 12:25:06 +0530

    CreateLead

---
+++ Day4/CreateLead.java
@@ -0,0 +1,32 @@
+package Week1.Day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateLead {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Manimozhi");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("M");
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_generalProfTitle")).sendKeys("TestLeafbrand");
+		driver.findElement(By.className("smallSubmit")).click();
+		Thread.sleep(3000);
+	
+		driver.close();
+		
+
+
+	}
+
+}
