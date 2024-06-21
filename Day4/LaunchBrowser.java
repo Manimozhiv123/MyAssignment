77b5d359d0a38ed43075a916745a995459b62bf4
Manimozhiv123 <manimozhi.pm41@gmail.com>
2024-06-21 11:34:08 +0530

    Launch Browser-Testleaf

---
+++ Day4/LaunchBrowser.java
@@ -0,0 +1,22 @@
+package Week1.Day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		Thread.sleep(2000);
+		driver.close();
+		
+	}
+
+}
